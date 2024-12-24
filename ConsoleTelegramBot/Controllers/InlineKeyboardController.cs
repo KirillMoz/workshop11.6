234406d3a35433b95f2f2804e64133a39ca6b619
KirillMoz <kirill.mozzhegorov@yandex.ru>
2024-12-24 20:52:03 +0500

    Промежуточное закрытие

---
+++ ConsoleTelegramBot/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using TelegramBot.Services;
+
+namespace TelegramBot.Controllers
+{
+    public class InlineKeyboardController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+        private readonly IStorage _memoryStorage;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+        }
+
+        [Obsolete]
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            if (callbackQuery?.Data == null)
+                return;
+
+            _memoryStorage.GetSession(callbackQuery.From.Id).FuctionCode = callbackQuery.Data;
+
+            /* switch (callbackQuery.Data)
+            {
+                case "GetCountSymbols":
+                    //await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
+                    break;
+            }*/
+
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
+        }
+    }
+}
---
+++ ConsoleTelegramBot/Controllers/TextMessageController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.Enums;
+using System.Collections.Generic;
+using TelegramBot.Services;
+using TelegramBot.Models;
+using TelegramBot.Utilities;
+
+namespace TelegramBot.Controllers
+{
+    public class TextMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+        private readonly IStorage _memoryStorage;
+
+        public TextMessageController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+        }
+
+        [Obsolete]
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            switch (message.Text)
+            {
+                case "/start":
+                    var screenButtons = new List<InlineKeyboardButton[]>();
+                    screenButtons.Add(new[] {InlineKeyboardButton.WithCallbackData($"Посчитать количество символов" , $"GetCountSymbols"),
+                                            InlineKeyboardButton.WithCallbackData($"Сложить числа" , $"Summary") });
+                    
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Тестовые функции ТГ бота на C#</b> {Environment.NewLine}", 
+                        cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(screenButtons));
+                    break;
+                default:
+                    string functionCode = _memoryStorage.GetSession(message.Chat.Id).FuctionCode;
+                    switch (functionCode)
+                    {
+                        case "GetCountSymbols":
+                            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Количество символов = {message.Text.Length}", cancellationToken: ct);
+                            break;
+                        case "Summary":
+                            int Summ = HelpString.GetSummary(message.Text, ' ');
+                            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Сумма чисел = {Summ}", cancellationToken: ct);
+                            break;
+                        default:
+                            await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Ошибка!!! Ввели неизвестную команду!!!", cancellationToken: ct);
+                            break;
+                     
+                    }
+                    break;
+            }
+        }
+    }
+}
